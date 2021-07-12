3d75aea5b9e7966dc86791a255eafe6d3523d175
Shirley Tang <shirleytang1996@gmail.com>
2021-07-12 19:56:14 -0400

    finish comb class

--- src/scripts/comb.js
+++ src/scripts/comb.js
@@ -1,6 +1,30 @@
 
 export default class Comb{
    constructor(){
-       
+       this.comb_btn=document.querySelector(".comb");
+       this.hair_obj = document.querySelector(".hair-style")
+       this.comb_btn.addEventListener('click',this.handleComb.bind(this))
+
+   }
+
+   handleComb(e){
+    e.preventDefault();
+    switch(this.hair_obj.id ){
+        case 'long-cut':
+            this.hair_obj.src="/src/image/hairstyle/long/median.png";
+            this.hair_obj.id ="median";
+            break;
+        case 'initial-cut':
+            this.hair_obj.src="/src/image/hairstyle/long/median.png";
+            this.hair_obj.id ="median";
+            break;
+        case 'curling-long-cut':
+            this.hair_obj.src="/src/image/hairstyle/curling/curling-median.png";
+            this.hair_obj.id ="curling-median";
+            break;
+        default:
+            alert("it's perfect");
+        
+        }
    }
 }
