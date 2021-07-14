f6bda733e59e7cbb3b966cb41ae89cae6568bef4
Shirley Tang <shirleytang1996@gmail.com>
2021-07-14 14:51:52 -0400

    finish limitdye

--- src/scripts/tools/limitDye.js
+++ src/scripts/tools/limitDye.js
@@ -1,3 +1,4 @@
+import { hexToCSSFilter } from 'hex-to-css-filter';
 export default class LimitDye{
     constructor(){
         console.log('limit dye')
@@ -35,12 +36,48 @@ export default class LimitDye{
         this.colordiv.appendChild(this.purplebtn);
 
         this.dye_btn.addEventListener('click',this.handleShow.bind(this))
-      
+
+        this.redbtn.addEventListener('click',this.handleRed.bind(this))
+        this.bluebtn.addEventListener('click',this.handleBlue.bind(this))
+        this.greenbtn.addEventListener('click',this.handleGreen.bind(this))
+        this.yellowbtn.addEventListener('click',this.handleYellow.bind(this));
+        this.purplebtn.addEventListener('click',this.handlePurple.bind(this))
     }
 
     handleShow(e){
         e.preventDefault();
-        this.colordiv.style.display = 'flex';
-       
+        this.colordiv.style.display = 'flex';   
     }
+
+    handleRed(e){
+        const cssFilter= hexToCSSFilter("#fc1a1a");
+        this.hair_obj.style.filter = cssFilter.filter.slice(0,-1);
+        this.bangs_obj.style.filter = cssFilter.filter.slice(0,-1);
+    }
+
+    handleBlue(e){
+        const cssFilter= hexToCSSFilter("#401afc");
+        this.hair_obj.style.filter = cssFilter.filter.slice(0,-1);
+        this.bangs_obj.style.filter = cssFilter.filter.slice(0,-1);
+    }
+
+    handleGreen(e){
+        const cssFilter= hexToCSSFilter("#1afc77");
+        this.hair_obj.style.filter = cssFilter.filter.slice(0,-1);
+        this.bangs_obj.style.filter = cssFilter.filter.slice(0,-1);
+    }
+
+    handleYellow(e){
+        const cssFilter= hexToCSSFilter("#fcdc1a");
+        this.hair_obj.style.filter = cssFilter.filter.slice(0,-1);
+        this.bangs_obj.style.filter = cssFilter.filter.slice(0,-1);
+    }
+
+    handlePurple(e){
+        const cssFilter= hexToCSSFilter("#a516a5");
+        this.hair_obj.style.filter = cssFilter.filter.slice(0,-1);
+        this.bangs_obj.style.filter = cssFilter.filter.slice(0,-1);
+    }
+
+    
 }
