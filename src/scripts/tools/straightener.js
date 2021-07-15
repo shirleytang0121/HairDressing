7d292ceba00ca02c32b43de575769d9aee48ec76
Shirley Tang <shirleytang1996@gmail.com>
2021-07-15 12:58:31 -0400

    update straightener for 2player mode

--- src/scripts/tools/straightener.js
+++ src/scripts/tools/straightener.js
@@ -2,8 +2,10 @@ export default class Straightener{
     constructor(){
         this.straightener_btn = Array.from(document.getElementsByClassName('straightener'));
         this.hair_obj = document.querySelector(".hair-style");
+        this.hair_obj_L = document.querySelector(".hair-style-L");
+
         if(this.straightener_btn.length > 1){
-            this.straightener_btn[0].addEventListener('keypress',this.handleKeys.bind(this))
+            this.straightener_btn[0].addEventListener('click',this.handleKeys.bind(this))
             this.straightener_btn[1].addEventListener('click',this.handleStraigtener.bind(this))
         }else{
             this.straightener_btn[0].addEventListener('click',this.handleStraigtener.bind(this))
@@ -11,8 +13,22 @@ export default class Straightener{
     }
 
     handleKeys(e){
-        console.log(e);
-        console.log('hi');
+        e.preventDefault();
+        
+        if(this.hair_obj_L.id === 'initial' || this.hair_obj_L.id==='long' ||this.hair_obj_L.id === 'curling-long'){
+           this.hair_obj_L.src="./src/image/hairstyle/straight/straight-long.png";
+           this.hair_obj_L.id ="straight-long";
+        }else if(this.hair_obj_L.id === 'curling-median' || this.hair_obj_L.id==='median' || this.hair_obj_L.id==='blow'){
+            this.hair_obj_L.src="./src/image/hairstyle/straight/straight-median.png";
+           this.hair_obj_L.id ="straight-median";
+        }else if(this.hair_obj_L.id === 'curling-short' || this.hair_obj_L.id === 'short'){
+            this.hair_obj_L.src="./src/image/hairstyle/straight/straight-short.png";
+            this.hair_obj_L.id ="straight-short";
+        }else if (this.hair_obj_L.id === 'long-cut'||this.hair_obj_L.id === 'long-cut-end' ||this.hair_obj_L.id ==='curling-long-cut'||this.hair_obj.id ==='curling-long-cut-end'||this.hair_obj.id ==='straight-long-cut'||this.hair_obj.id ==='straight-long-cut-end'){
+            alert('Please comb your hair first')
+        }else{
+            alert('it is perfect');
+        }
     }
     
     handleStraigtener=function(e){
